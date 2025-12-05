3e40053fd8bcb079607820321d5549c238d107ae
Florian Knauf <knauf@vss.uni-hannover.de>
2025-12-05 16:13:56 +0100

    log requested events on the plain button

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,19 @@ import { ActionButton } from "@adobe/react-spectrum";
 
 export default function Home() {
   return <>
-    <button data-cy="plain-btn">
+    <button
+      data-cy="plain-btn"
+      onBlur={console.log}
+      onClick={console.log}
+      onFocus={console.log}
+      onMouseEnter={console.log}
+      onMouseOver={console.log}
+      onMouseDown={console.log}
+      onMouseUp={console.log}
+      onMouseLeave={console.log}
+      onMouseMove={console.log}
+      onMouseOut={console.log}
+    >
       Plain
     </button>
     <ActionButton data-cy="spectrum-btn">
